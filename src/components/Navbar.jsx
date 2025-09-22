08fa583950fa5c38b6e03edc4d8bc99c971a0d59
ravi-756 <ravidhundhara2000.gmail>
2025-09-22 15:08:22 +0530

    testing done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Company Logo / Name */}
         <h1 className="text-2xl font-extrabold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-          Megacode
+          MegacodeiT
         </h1>
 
         {/* Navigation Links */}
